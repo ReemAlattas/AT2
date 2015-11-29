6bda24cf0b030e9ccdaeb101ac47984e191630af
ReemAlattas <ralattas@live.com>
2015-11-29 01:10:54 -0500

    make it better

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,16 @@
 
 ActiveRecord::Schema.define(version: 20151128010611) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "actions", force: :cascade do |t|
+    t.text     "description"
+    t.string   "target"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
   create_table "acts", force: :cascade do |t|
     t.string   "desc"
     t.string   "target"
@@ -21,7 +31,38 @@ ActiveRecord::Schema.define(version: 20151128010611) do
     t.integer  "kpi_id"
   end
 
-  add_index "acts", ["kpi_id"], name: "index_acts_on_kpi_id"
+  add_index "acts", ["kpi_id"], name: "index_acts_on_kpi_id", using: :btree
+
+  create_table "authorizations", force: :cascade do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "companies", force: :cascade do |t|
+    t.string   "name"
+    t.string   "website"
+    t.string   "contactName"
+    t.string   "contactEmail"
+    t.string   "contactPhone"
+    t.text     "bio"
+    t.string   "video"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+    t.string   "logo_file_name"
+    t.string   "logo_content_type"
+    t.integer  "logo_file_size"
+    t.datetime "logo_updated_at"
+  end
+
+  create_table "kactions", force: :cascade do |t|
+    t.text     "description"
+    t.string   "target"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "kpis", force: :cascade do |t|
     t.string   "description"
@@ -32,12 +73,65 @@ ActiveRecord::Schema.define(version: 20151128010611) do
     t.integer  "plan_id"
   end
 
-  add_index "kpis", ["plan_id"], name: "index_kpis_on_plan_id"
+  add_index "kpis", ["plan_id"], name: "index_kpis_on_plan_id", using: :btree
 
   create_table "plans", force: :cascade do |t|
     t.string   "goal"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "pusers", force: :cascade do |t|
+    t.boolean  "admin"
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "pusers", ["email"], name: "index_pusers_on_email", unique: true, using: :btree
+  add_index "pusers", ["reset_password_token"], name: "index_pusers_on_reset_password_token", unique: true, using: :btree
+
+  create_table "talents", force: :cascade do |t|
+    t.text     "FName"
+    t.text     "LName"
+    t.text     "email"
+    t.text     "portfolioLink"
+    t.text     "linkedinLink"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.string   "resume_file_name"
+    t.string   "resume_content_type"
+    t.integer  "resume_file_size"
+    t.datetime "resume_updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.string   "provider"
+    t.string   "uid"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
